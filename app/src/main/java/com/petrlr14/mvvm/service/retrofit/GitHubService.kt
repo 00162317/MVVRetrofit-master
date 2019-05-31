e242d7681aa9af6b1b51414b7b6ece68eb8b1a94
Roberto <00162317@uca.edu.sv>
2019-05-31 13:54:16 -0600

    Se creo la funcion.

--- app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
@@ -1,4 +1,8 @@
 package com.petrlr14.mvvm.service.retrofit
 
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+
 interface GitHubService {
+
+    fun getRepos(user:String): List<GitHubRepo>
 }
