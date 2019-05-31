8dcd4897e715a1318f679e5834dd53a30f54800a
Roberto <00162317@uca.edu.sv>
2019-05-31 13:59:15 -0600

    Response: Indico la respuesta

--- app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
@@ -1,12 +1,13 @@
 package com.petrlr14.mvvm.service.retrofit
 
 import com.petrlr14.mvvm.database.entities.GitHubRepo
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
 
     @GET("/users/{user}/repos")
-    fun getRepos(@Path("user")user:String): List<GitHubRepo>
+    fun getRepos(@Path("user")user:String): Response<List<GitHubRepo>>
 
 }
