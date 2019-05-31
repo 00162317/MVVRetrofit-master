79a064f45bd21cf40d8e16f35cb3fced5554e60a
Roberto <00162317@uca.edu.sv>
2019-05-31 13:56:08 -0600

    Creo una ruta!!

--- app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
@@ -1,8 +1,10 @@
 package com.petrlr14.mvvm.service.retrofit
 
 import com.petrlr14.mvvm.database.entities.GitHubRepo
+import retrofit2.http.GET
 
 interface GitHubService {
 
+    @GET("/users/{user}/repos")
     fun getRepos(user:String): List<GitHubRepo>
 }
