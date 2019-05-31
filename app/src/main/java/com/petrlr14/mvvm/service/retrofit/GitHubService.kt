0a0dc299e78fd1a2b0206a23af1e491793d3c4ff
Roberto <00162317@uca.edu.sv>
2019-05-31 13:56:54 -0600

    En el path pondremos el nombre o mas bien la variable de USER

--- app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
@@ -2,9 +2,10 @@ package com.petrlr14.mvvm.service.retrofit
 
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GitHubService {
 
     @GET("/users/{user}/repos")
-    fun getRepos(user:String): List<GitHubRepo>
+    fun getRepos(@Path("user")user:String): List<GitHubRepo>
 }
